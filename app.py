0f17e9fdfee1c1915bb7c570f6859d739bea060e
klimrus61 <klimrus61@mail.ru>
2022-09-30 11:29:04 +0300

    search

--- app.py
+++ app.py
@@ -7,16 +7,17 @@ def index():
     """
     TODO: Render the home page provided under templates/index.html in the repository
     """
-    return "TODO"
+    return render_template('index.html')
 
 @app.get("/search")
 def search():
-	"""
+    """
 	TODO:
 	1. Capture the word that is being searched
 	2. Seach for the word on Google and display results
 	"""
-    return "TODO"
+    query_string = request.args.get("q")
+    return redirect(f"https://google.com/search?q={query_string}")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
